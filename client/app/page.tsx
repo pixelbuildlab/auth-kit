ba7d1f47b6ee6ff244cbda6669a02ed64a053a20
pixelbuildlab <2017n2801@gmail.com>
2024-12-05 17:35:56 +0500

    fixed typo

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             >
               Try
             </Link>
-            <a href='/api/auth/login'>12Login</a>
+            <a href='/api/auth/login'>Auth0 Login</a>
 
             {/* <a
               href='#features'
