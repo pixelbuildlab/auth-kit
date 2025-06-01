5f00fe80d90547b5c5811c44f460bad5db0261ec
pixebuildlab <2017n2801@gmail.com>
2025-06-02 03:23:35 +0500

    added stripe docs

--- client/lib/stripe.ts
+++ client/lib/stripe.ts
@@ -4,5 +4,5 @@ import Stripe from 'stripe'
 
 const config = getConfig()
 const { serverRuntimeConfig } = config || {}
-
+// https://docs.stripe.com/checkout/quickstart?lang=node&client=next
 export const stripe = new Stripe(serverRuntimeConfig?.stripeSecretKey)
